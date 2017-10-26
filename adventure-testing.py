1be5129ef6d16154be8c97a08455198564b37005
Divvox <32779641+Divvox@users.noreply.github.com>
2017-10-26 15:18:06 -0700

    added functions and consolidated .json files
    added a help function, consolidated help.json with other .json objects so I don't have to open as many.  Cleaned up several processes to select options.

--- adventure-testing.py
+++ adventure-testing.py
@@ -1,15 +1,14 @@
 import random
-import csv
 import json
 import textwrap
 
-
+# opening important files...
 with open('rooms.json') as room_file:
     rooms = json.load(room_file)
 
 
-with open('help.json') as help_file:
-    help = json.load(help_file)
+with open('Main_Reference.json') as ref_file:
+    ref = json.load(ref_file)
 # Takes a room ID and column name from the rooms.csv file and returns the requested field!
 
 
@@ -34,51 +33,71 @@ def rrecord(rid):
             return rooms["rooms"][i]
 
 
+def move_error():
+    r = str(random.randrange(1,5))
+    return ref["moveerrors"][r]
+
+
+# Generates a list of available exits for the indicated room.
+def exits(rid):
+    ex = ""
+    for i in rooms["rooms"][rid]["exitdesc"].values():
+        ex = ex + "\n" + i
+    return ex
+
+
+# Starts the game!
 def start_game():
     """Entering loop to allow for all the player commands"""
     # name = input("Who are you? (This is case sensitive) ")
     p = Player('Bob_Test', rfield("0001", "rid"), {})
     print("Hello " + str(p.player_name) + "!")
-    print(textwrap.fill(rfield(p.cur_room, "short_desc"), 80))
+    print(textwrap.fill(rfield(p.cur_room, "short_desc"), 100))
     while True:
-        action = ""
         print()
-        action = input().lower().rstrip()
+        action = input(">>>").lower().rstrip()
         if action.startswith('help'):
+            text = action.split(" ")
             if len(text) == 1:
-                print(textwrap.fill(help["general"]))
+                for i in ref["help"]:
+                    print(textwrap.fill(ref["help"][i], 100) + "\n")
+                    continue
             else:
-                text = action.split(" ")
                 try:
-                    print(textwrap.fill(help[text[1]]))
+                    print(textwrap.fill(ref["help"][text[1]]), 100)
                 except KeyError:
                     print("No help record found for " + text[1])
+        elif action.startswith('exits'):
+            try:
+                print(textwrap.fill(exits(p.cur_room)), 100)
+            except KeyError:
+                print("Error: exit descriptions not provided for this room, poke Sam in the side.")
         elif action.startswith('go') or action.startswith('enter'):
             text = action.split(" ")
             try:
                 p.cur_room = rooms["rooms"][p.cur_room]["exits"][text[1]]
-                print(textwrap.fill(rfield(p.cur_room, "short_desc")))
+                print(textwrap.fill(rfield(p.cur_room, "short_desc")), 100)
                 continue
             except KeyError:
-                print("I would if I could.  Maybe choose a direct with a way through.")
+                print(move_error())
             continue
         elif action.startswith('look'):
             text = action.split(" ")
             if len(text) == 1:
-                print(textwrap.fill(rfield(p.cur_room, "full_desc")))
+                print(textwrap.fill(rfield(p.cur_room, "full_desc")), 100)
                 continue
             else:
                 try:
-                    print(textwrap.fill(rooms["rooms"][rooms["rooms"][p.cur_room]["exits"][text[1]]]["look_desc"]))
+                    print(textwrap.fill(rooms["rooms"][rooms["rooms"][p.cur_room]["exits"][text[1]]]["look_desc"]), 100)
                     continue
                 except KeyError:
                     print("Looks like a wall, ya' derp. Choose something else to look at.")
                     continue
-            continue
         elif action.startswith('quit'):
             print("Game over man!")
             break
         else:
             print("I'm sorry, I don't understand that command.")
+
 
 start_game()
